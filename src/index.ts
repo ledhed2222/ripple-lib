0f35d3d4cf530f94bf53745f130a2b4ad30e2b94
Mayukha Vadari <mvadari@ripple.com>
2021-08-17 17:52:05 -0700

    rename ClientBroadcast -> BroadcastClient

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ export * from './models'
 export * from './offline/utils'
 
 // Broadcast client is experimental
-export {ClientBroadcast} from './broadcast'
+export {BroadcastClient} from './client/broadcast'
 
 export * from './Wallet'
