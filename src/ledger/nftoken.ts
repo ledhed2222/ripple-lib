953eb7916f93048672f0e7769e4bfcc7abcdf380
Omar Khan <khancodegt@gmail.com>
2021-09-14 19:27:58 -0400

    cleanup regex

--- src/ledger/nftoken.ts
+++ src/ledger/nftoken.ts
@@ -43,7 +43,7 @@ const validateOnLedger = (
 }
 
 const isDataUriValid = (uri: string): boolean => {
-  const regex = /data:([-\w]+\/[-+\w.]+)?(;?[\w]+=[-\w]+)*(;base64)?,.*/gu
+  const regex = /data:([-\w]+\/[-+\w.]+)?(;?\w+=[-\w]+)*(;base64)?,.*/gu
   return regex.test(uri)
 }
 
